bdd5e9a2ae4c84ac2e32ee133cd67f0105a43da5
yterraillon <yterraillon@gmail.com>
2022-01-16 10:13:48 +0100

    Hello world in a handler

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,6 +1,20 @@
+/// <summary>
+/// Based on https://medium.com/geekculture/minimal-apis-in-net-6-a-complete-guide-beginners-advanced-fd64f4da07f5
+/// </summary>
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+app.MapGet("/", MyHandler.Hello);
+
+app.Urls.Add("http://localhost:3000");
+app.Urls.Add("http://localhost:4000");
 
 app.Run();
+
+class MyHandler {
+
+    public static string Hello()
+    { return "Hello World!"; }
+}
