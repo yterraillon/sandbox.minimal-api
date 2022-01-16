a1ea419fcb082cf508d6e758f62d2f16192c56c9
yterraillon <yterraillon@gmail.com>
2022-01-16 11:12:54 +0100

    EF migration + Create/Read api

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,20 +1,43 @@
 /// <summary>
-/// Based on https://medium.com/geekculture/minimal-apis-in-net-6-a-complete-guide-beginners-advanced-fd64f4da07f5
+/// Based on the following examples : 
+/// https://medium.com/geekculture/minimal-apis-in-net-6-a-complete-guide-beginners-advanced-fd64f4da07f5
+/// https://github.com/davidfowl/CommunityStandUpMinimalAPI
+/// https://www.hanselman.com/blog/exploring-a-minimal-web-api-with-aspnet-core-6
 /// </summary>
-
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("Todos") ?? "Data Source=Todos.db";
+builder.Services.AddSqlite<TodoDbContext>(connectionString);
+
+builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-//app.MapGet("/", () => "Hello World!");
-app.MapGet("/", MyHandler.Hello);
-
-app.Urls.Add("http://localhost:3000");
-app.Urls.Add("http://localhost:4000");
-
-app.Run();
-
-class MyHandler {
-
-    public static string Hello()
-    { return "Hello World!"; }
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseSwagger(); 
+
+app.MapGet("/todos", async (TodoDbContext db) => await db.Todos.ToListAsync());
+
+app.MapGet("/todos/{id}", async (TodoDbContext db, int id) =>
+    await db.Todos.FindAsync(id) switch
+        {
+            Todo todo => Results.Ok(todo),
+            null => Results.NotFound()
+        }
+);
+
+app.MapPost("/todos", async (TodoDbContext db, Todo todo) =>
+{
+    await db.Todos.AddAsync(todo);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/todos/{todo.Id}", todo);
+});
+
+app.UseSwaggerUI();
+app.Run();
---
+++ src/Api/TodoDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+/// <summary>
+/// EF Setup : 
+/// https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/migrations?view=aspnetcore-6.0
+/// 
+/// Had to : 
+/// 1- dotnet tool install dotnet-ef -g
+/// 2- reference Microsoft.EntityFrameworkCore.Design in csproj
+/// 3- dotnet ef migrations add InitialCreate
+/// 4- dotnet-ef database update
+/// </summary>
+public class TodoDbContext : DbContext
+{
+    public TodoDbContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
+
+public class Todo
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Title { get; set; } = string.Empty;
+    public bool IsComplete { get; set; }
+}
